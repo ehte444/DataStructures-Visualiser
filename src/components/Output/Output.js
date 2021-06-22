c9f51b087dd47c926e7c47ec6f16b58ce8d516cb
Ehtesham Siddiqui <siddiquie.ehtesham@gmail.com>
2021-06-22 17:08:25 +0530

    Respnsive Tweakings

--- src/components/Output/Output.js
+++ src/components/Output/Output.js
@@ -3,7 +3,7 @@ import './output.css';
 
 function Output({placeholder}) {
   return (
-    <div className=" output">
+    <div className="output">
       <p className="mt-2">Output :-</p>
       <input className="form-control p-3" type="text" placeholder={placeholder} aria-label="Disabled input example" disabled readOnly/>
     </div>
